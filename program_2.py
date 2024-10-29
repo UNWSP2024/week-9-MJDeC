c472efa71fbdbd6358a5f8dad8024bd4f1de5dd2
MJDeC <mjdecaro@students.unwsp.edu>
2024-10-29 18:28:05 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -3,3 +3,13 @@
 # Each random number should be in the range of 1 through 500. 
 # The application should let the user specify how many random numbers the file will hold 
 # (up to 1000).
+import random
+def main():
+  user_input=int(input('Enter the integer number of random numbers to be generated.'))
+  outfile=open('rand.txt','w')
+  for count in range(user_input):
+    randnumb=random.randint(1,500)
+    outfile.write(str(randnumb)+'\n')
+  outfile.close()
+
+main()
